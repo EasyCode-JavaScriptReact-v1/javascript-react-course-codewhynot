c1d2bc25dc583a697183b58b665134be8cefbe8d
markupus_mir <miroslav@markupus.tech>
2018-06-05 17:00:14 +0300

    fix Math floor

--- js-core/homeworks/homework-2/src/main.js
+++ js-core/homeworks/homework-2/src/main.js
@@ -68,7 +68,8 @@ console.log(myObj)
  */
 
 let frameworks = [4.7, 'Angular', '6Angular', 'React/Redux'];
-let x =  `google released new version ${frameworks[1]} ${Math.floor(frameworks[0])} But real speed is ${frameworks[frameworks.length - 1]}`;
+let numb = Math.floor(frameworks[0])
+let x =  `google released new version ${frameworks[1]} ${numb}. But real speed is ${frameworks.slice(-1)}`;
 console.log(x)
 
 // LINKS
